094e10aa1da54421267b53746daa29eb6ef34eed
ellejaiebaie <48771627+laurajbrennan@users.noreply.github.com>
2020-03-23 12:22:39 -0700

    adding message under an item works, now need them to display

--- client/src/components/pages/Item.js
+++ client/src/components/pages/Item.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { UserContext } from "../../contexts/UserContext";
+import { Link } from "react-router-dom";
+import Message from "./Message";
 
 export class Item extends Component {
-  state = { loading: true, user: {}, item: {} };
+  state = { loading: true, user: {}, item: {}, message: false };
   static contextType = UserContext;
 
   componentDidMount() {
@@ -18,6 +20,10 @@ export class Item extends Component {
     });
   }
 
+  showMessage = () => {
+    this.setState({ message: true });
+  };
+
   render() {
     if (this.state.loading) {
       return (
@@ -29,7 +35,35 @@ export class Item extends Component {
       return (
         <>
           <section className="item" key={this.state.item.id}>
-            <p>{this.state.item.title}</p>
+            <header className="item__header">
+              {this.state.item.type === "OFFERED: " ? (
+                <div className="item__icon--offered"></div>
+              ) : (
+                <div className="item__icon--wanted"></div>
+              )}
+              <span>{this.state.item.title}</span>
+            </header>
+            <main className="item__body">
+              <p>{this.state.item.description}</p>
+              <div className="item__footer">
+                <span>{this.state.item.area}</span>
+              </div>
+              <div className="item__message-container">
+                {this.state.user.id === "" ? (
+                  <Link to="/login">
+                    <button>Send a message</button>
+                  </Link>
+                ) : (
+                  <button onClick={this.showMessage}>Send a message</button>
+                )}
+              </div>
+              <div
+                className="item__message"
+                display={this.state.message === false ? "none" : "block"}
+              >
+                <Message user={this.state.user} item={this.state.item} />
+              </div>
+            </main>
           </section>
         </>
       );
--- client/src/components/pages/Message.js
+++ client/src/components/pages/Message.js
@@ -1,15 +1,39 @@
-import React, { Component } from "react";
-import InputMessage from "../InputMessage";
+import React from "react";
+import axios from "axios";
+import { UserContext } from "../../contexts/UserContext";
 
-export class Message extends Component {
-  state = { isLoggedIn: false, loading: true, user: {}, items: [] };
-  render() {
-    return (
-      <div>
-        <InputMessage />
+export default function Message(props) {
+  const postMessage = e => {
+    e.preventDefault();
+    let newMessage = {
+      sentBy: props.user.username,
+      item: props.item.id,
+      itemOwner: props.item.ownedBy,
+      text: e.target.message.value
+    };
+    axios({
+      method: "put",
+      url: `http://localhost:5000/items/${props.item.id}`,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      data: newMessage 
+    });
+    document.querySelector(".message__form").reset();
+  };
+  return (
+    <div>
+      <div className="message__input">
+        <form action="submit" className="message__form" onSubmit={postMessage}>
+          <textarea
+            className="message__text"
+            type="textarea"
+            name="message"
+            placeholder="Type your message to the post's author here"
+          ></textarea>
+          <button type="submit" className="message__submit">
+            Send
+          </button>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Message;
